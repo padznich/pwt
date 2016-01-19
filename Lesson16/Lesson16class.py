0a03637829740af4d828e85687ec97a68d4f836a
padznich <slabkopg@gmail.com>
2016-01-19 21:25:00 +0300

    16

---
+++ Lesson16/Lesson16class.py
@@ -0,0 +1,15 @@
+# https://www.digitalocean.com/community/tutorials/how-to-use-mysql-or-mariadb-with-your-django-application-on-ubuntu-14-04
+
+# http://www.bogotobogo.com/python/Django/Python_Django_hello_world.php
+
+# https://docs.djangoproject.com/en/1.8/intro/tutorial01/
+'''
+python manage.py sqlmigrate <app_name> ooo1 # 0001 - some_id
+
+python manage.py makemigrations
+python manage.py migrate
+
+python manage.py shell
+
+>>> from <app_name>.models import <table_name>
+'''
